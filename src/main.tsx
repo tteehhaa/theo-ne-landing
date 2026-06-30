34fdfecdc63005ae30ee7e4dde4d63692e8a1cc0
tteehhaa <hanabeom@gmail.com>
2026-06-30 21:00:48 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './i18n' // ★ 이 줄을 반드시 추가해야 다국어가 작동합니다!
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
